4380b04dcd78f9021cce5684291aaf2a0c388e78
why-project96 <91371555+why-project96@users.noreply.github.com>
2022-09-27 17:34:24 +0700

    Add files via upload

---
+++ v2ray/cert.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain="$(cat /etc/v2ray/domain)"
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+bash acme.sh --register-account -m why-project96@gmail.com
+bash acme.sh --issue --standalone -d $domain
+bash acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key
+/etc/init.d/ssh restart
+/etc/init.d/dropbear restart
+/etc/init.d/stunnel4 restart
+/etc/init.d/openvpn restart
+/etc/init.d/fail2ban restart
+/etc/init.d/cron restart
+/etc/init.d/nginx restart
+/etc/init.d/squid restart
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000
+systemctl restart v2ray@none
+systemctl restart v2ray
+systemctl restart trojan
+systemctl restart v2ray@vless
+systemctl restart v2ray@vnone
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "          Service/s Restarted         "
+                echo -e ""
+                echo -e "======================================"
+                exit
+clear 
+neofetch
